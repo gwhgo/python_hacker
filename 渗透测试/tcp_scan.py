e359597d0a2da1819d2e9b2e38ccff2b806371e9
gwh <guoweihao_shanke@163.com>
2018-06-01 15:42:15 +0800

    add 扫描器

---
+++ 渗透测试/tcp_scan.py
@@ -0,0 +1,50 @@
+import optparse
+from socket import *
+from threading import *
+screenLock = Semaphore(value=1)
+def connScan(tgtHost,tgtPort):
+    try:
+        connSkt = socket(AF_INET,SOCK_STREAM)
+        connSkt.connect((tgtHost,tgtPort))
+        #connSkt.send('ViolentPython\r\n')
+        result = connSkt.recv(100)
+        screenLock.acquire()
+        print("[+]%d/tcp open" % tgtPort)
+        print("[+] "+ str(result))
+    except Exception as e:
+        screenLock.acquire()
+        print("[-]%d/tcp closed." % tgtPort)
+    finally:
+        screenLock.release()
+        connSkt.close()
+
+def portScan(tgtHost,tgtPorts):
+    try:
+        tgtIP = gethostbyname(tgtHost)
+    except:
+        print("[-] Cannot resolve '%s':Unknown host"%tgtHost)
+        return 
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print("\n[+] Scan Results for: " + tgtName[0])
+    except:
+        print("\n[+] Scan Result for: " + tgtIP)
+    setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
+        t = Thread(target=connScan,args=(tgtHost,int(tgtPort)))
+        t.start()
+
+def main():
+    parser = optparse.OptionParser('usage %prog -H' + '<target host> -p <target port>')
+    parser.add_option('-H',dest='tgtHost',type='string',help='specify target host')
+    parser.add_option('-p',dest='tgtPorts',type='string',help='specify target port')
+    (options,args) = parser.parse_args()
+    tgtHost = options.tgtHost
+    tgtPorts = str(options.tgtPorts).split(',')
+    if (tgtHost == None) | (options.tgtPorts == None):
+        print(parser.usage)
+        exit(0)
+    portScan(tgtHost,tgtPorts)
+
+if __name__ == "__main__":
+    main()
